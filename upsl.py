6cec1daef48b17330a31b6f38bd88d2126dfb381
Devin Dow <DevinDow@gmail.com>
2018-07-29 22:11:14 +0000

    output

--- upsl.py
+++ upsl.py
@@ -2,15 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# File Contents
+# Input
 #url_page = "http://www.upslsoccer.com/schedule"
 #content = requests.get(url_page).content
 
-file = open('upsl/a.html', 'r')
-content = file.read()
-file.close()
+input = open('upsl/a.html', 'r')
+content = input.read()
+input.close()
 
 #print content
+
+
+# Write Output File
+output = open ('upsl/output.html', 'w')
+output.write("<html>\n")
+output.write("\t<body>\n")
+output.write("\t\t<table>\n")
 
 
 # Beautiful Soup
@@ -22,5 +29,13 @@ for tr in soup.table.find_all('tr'):
     #print tr.select('td.schedule_venueName')
     tdVenue = tr.select('td.schedule_venueName')
     if (len(tdVenue) > 0):
-        print tr.prettify()
+        print tdVenue[0]
+        #print tr.prettify()
+        output.write(tr.prettify())
     
+
+# Close Output File
+output.write("\t\t</table>\n")
+output.write("\t</body>\n")
+output.write("</html>\n")
+output.close()
